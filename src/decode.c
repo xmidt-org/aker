500d4a060e7529f67ad49097c645acba9e8a7e5c
mander207 <Minas_Anderiasian@cable.comcast.com>
2017-10-23 20:12:21 -0700

    decode_schedule() is now complete.

--- src/decode.c
+++ src/decode.c
@@ -19,27 +19,8 @@
 
 #include "schedule.h"
 #include "decode.h"
+#include "aker_log.h"
 
-/* The test message
-{
-    "weekly-schedule": [
-        { "time": 10, "indexes": [0, 1, 3] },
-        { "time": 20, "indexes": [0] },
-        { "time": 30, "indexes": [] }
-    ],
-
-    "macs": [
-        "11:22:33:44:55:aa",
-        "22:33:44:55:66:bb",
-        "33:44:55:66:77:cc",
-        "44:55:66:77:88:dd"
-    ],
-
-    "absolute-schedule": [
-        { "unix-time": 1508213527, "indexes": [0, 2] }
-    ]
-}
-*/
 
 #define WEEKLY_SCHEDULE   "weekly-schedule"
 #define MACS              "macs"
@@ -53,36 +34,7 @@ static int decode_macs_table       (msgpack_object *key, msgpack_object *val, sc
 
 static int process_map(msgpack_object_map *, schedule_event_t **t);
 
-/* DEBUG ONLY */
-char test_buffer[] = {
-0x83, 0xAF, 0x77, 0x65,   0x65, 0x6B, 0x6C, 0x79,   0x2D, 0x73, 0x63, 0x68,
-0x65, 0x64, 0x75, 0x6C,   0x65, 0x93, 0x82, 0xA4,   0x74, 0x69, 0x6D, 0x65,
-0x0A, 0xA7, 0x69, 0x6E,   0x64, 0x65, 0x78, 0x65,   0x73, 0x93, 0x00, 0x01,
-0x03, 0x82, 0xA4, 0x74,   0x69, 0x6D, 0x65, 0x14,   0xA7, 0x69, 0x6E, 0x64,
-0x65, 0x78, 0x65, 0x73,   0x91, 0x00, 0x82, 0xA4,   0x74, 0x69, 0x6D, 0x65,
-0x1E, 0xA7, 0x69, 0x6E,   0x64, 0x65, 0x78, 0x65,   0x73, 0x90, 0xA4, 0x6D,
-0x61, 0x63, 0x73, 0x94,   0xB1, 0x31, 0x31, 0x3A,   0x32, 0x32, 0x3A, 0x33,
-0x33, 0x3A, 0x34, 0x34,   0x3A, 0x35, 0x35, 0x3A,   0x61, 0x61, 0xB1, 0x32,
-0x32, 0x3A, 0x33, 0x33,   0x3A, 0x34, 0x34, 0x3A,   0x35, 0x35, 0x3A, 0x36,
-0x36, 0x3A, 0x62, 0x62,   0xB1, 0x33, 0x33, 0x3A,   0x34, 0x34, 0x3A, 0x35,
-0x35, 0x3A, 0x36, 0x36,   0x3A, 0x37, 0x37, 0x3A,   0x63, 0x63, 0xB1, 0x34,
-0x34, 0x3A, 0x35, 0x35,   0x3A, 0x36, 0x36, 0x3A,   0x37, 0x37, 0x3A, 0x38,
-0x38, 0x3A, 0x64, 0x64,   0xB1, 0x61, 0x62, 0x73,   0x6F, 0x6C, 0x75, 0x74,
-0x65, 0x2D, 0x73, 0x63,   0x68, 0x65, 0x64, 0x75,   0x6C, 0x65, 0x91, 0x82,
-0xA9, 0x75, 0x6E, 0x69,   0x78, 0x2D, 0x74, 0x69,   0x6D, 0x65, 0xCE, 0x59,
-0xE5, 0x83, 0x17, 0xA7,   0x69, 0x6E, 0x64, 0x65,   0x78, 0x65, 0x73, 0x92,
-0x00, 0x02
-};
-void debug_code(void)
-{
-    schedule_t *t;
-    decode_schedule(sizeof(test_buffer), (uint8_t *) test_buffer, &t);
-}
-
-/* END DEBUG ONLY */
-
 int decode_schedule(size_t len, uint8_t * buf, schedule_t **t) {
-    /* buf is allocated by client. */
     msgpack_unpacked result;
     size_t off = 0;
     msgpack_unpack_return ret;
@@ -124,7 +76,8 @@ int decode_schedule(size_t len, uint8_t * buf, schedule_t **t) {
                 printf("Found %s\n", MACS);
                 decode_macs_table(key, val, &s);
                 } else {
-                // zztop handle_decode_error();
+                    debug_error("decode_schedule() can't handle object type\n");
+                    assert(0);
                 }
             p++;
             key = &p->key;
@@ -136,10 +89,10 @@ int decode_schedule(size_t len, uint8_t * buf, schedule_t **t) {
         msgpack_unpacked_destroy(&result);
 
         if (ret == MSGPACK_UNPACK_CONTINUE) {
-            printf("All msgpack_object in the buffer is consumed.\n");
+            debug_info("All msgpack_object in the buffer is consumed.\n");
         }
         else if (ret == MSGPACK_UNPACK_PARSE_ERROR) {
-            printf("The data in the buf is invalid format.\n");
+            debug_error("The data in the buf is invalid format.\n");
         }
     }
     return 0;
@@ -173,15 +126,17 @@ int decode_schedule_table (msgpack_object *key, msgpack_object *val, schedule_ev
 int decode_macs_table (msgpack_object *key, msgpack_object *val, schedule_t **t)
 {
     uint32_t i;
+    uint32_t count;
     msgpack_object *ptr = val->via.array.ptr;
+    mac_address *macs;
     (void ) key;
-    (void ) t;
     
-    for (i =0; i < (val->via.array.size);i++) {
-        char buf[128];
-        memset(buf, 0, 128);
-        memcpy(buf, ptr->via.str.ptr, ptr->via.str.size);
-        printf("MAC Table index %u is %s\n", i, buf);
+    count = val->via.array.size;
+    create_mac_table( *t, count );
+    macs = (*t)->macs;
+    
+    for (i =0; i < count;i++) {
+        memcpy(macs[i].mac, ptr->via.str.ptr, ptr->via.str.size);
         ptr++;
     }
     
@@ -203,14 +158,14 @@ int process_map(msgpack_object_map *map, schedule_event_t **t)
             memset(buf, 0, 64);
              memcpy(buf, key->via.str.ptr, key->via.str.size);
              entry_time = val->via.u64;
-             printf("Key val %s is %d\n", buf, (uint32_t ) val->via.u64);
+             debug_info("Key val %s is %d\n", buf, (uint32_t ) val->via.u64);
         } else if (key->type == MSGPACK_OBJECT_STR && val->type == MSGPACK_OBJECT_ARRAY) {
             msgpack_object *ptr = val->via.array.ptr;
             uint32_t array_size = 0;
             *t = create_schedule_event(val->via.array.size);
             for (;array_size < (val->via.array.size); array_size++) {
                 (*t)->block[array_size] = ptr->via.u64;
-                printf("Array Element[%d] = %d block[] %d\n", array_size, (uint32_t) ptr->via.u64, (*t)->block[array_size]);
+                debug_info("Array Element[%d] = %d block[] %d\n", array_size, (uint32_t) ptr->via.u64, (*t)->block[array_size]);
                 ptr++;
             }
             
