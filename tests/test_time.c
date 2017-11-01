1e201c1075fa2a1008565f4137cffcc98b0bd410
Ramki Ananthakrishnan <ramki_ananthakrishnan@cable.comcast.com>
2017-11-01 15:04:43 -0700

    Added unit tests for time functions.

---
+++ tests/test_time.c
@@ -0,0 +1,103 @@
+/**
+ *  Copyright 2017 Comcast Cable Communications Management, LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <wrp-c.h>
+
+#include <CUnit/Basic.h>
+
+#include "../src/time.h"
+
+/*----------------------------------------------------------------------------*/
+/*                                   Macros                                   */
+/*----------------------------------------------------------------------------*/
+/* none */
+
+/*----------------------------------------------------------------------------*/
+/*                               Data Structures                              */
+/*----------------------------------------------------------------------------*/
+/* none */
+
+/*----------------------------------------------------------------------------*/
+/*                            File Scoped Variables                           */
+/*----------------------------------------------------------------------------*/
+/* none */
+
+/*----------------------------------------------------------------------------*/
+/*                                   Mocks                                    */
+/*----------------------------------------------------------------------------*/
+/* None */
+
+/*----------------------------------------------------------------------------*/
+/*                                   Tests                                    */
+/*----------------------------------------------------------------------------*/
+void test_time_conversions()
+{
+    struct timespec tm;
+    time_t current_unix_time = 0, current_weekly_time = 0;
+    time_t expected_unix_time = 0, expected_weekly_time = 0;
+
+    if( 0 == clock_gettime(CLOCK_REALTIME, &tm) ) {
+        current_unix_time = tm.tv_sec;
+
+        current_weekly_time = convert_unix_time_to_weekly(current_unix_time);
+        expected_unix_time  = convert_weekly_time_to_unix(current_weekly_time);
+        CU_ASSERT(expected_unix_time == current_unix_time);
+
+        expected_weekly_time = convert_unix_time_to_weekly(expected_unix_time);
+        CU_ASSERT(expected_weekly_time == current_weekly_time);
+    } else {
+        CU_FAIL("Current time unavailable!!!\n");
+    }
+}
+
+void add_suites( CU_pSuite *suite )
+{
+    printf("--------Start of Test Cases Execution ---------\n");
+    *suite = CU_add_suite( "tests", NULL, NULL );
+    CU_add_test( *suite, "Test 1", test_time_conversions );
+}
+
+/*----------------------------------------------------------------------------*/
+/*                             External Functions                             */
+/*----------------------------------------------------------------------------*/
+int main( void )
+{
+    unsigned rv = 1;
+    CU_pSuite suite = NULL;
+
+    if( CUE_SUCCESS == CU_initialize_registry() ) {
+        add_suites( &suite );
+
+        if( NULL != suite ) {
+            CU_basic_set_mode( CU_BRM_VERBOSE );
+            CU_basic_run_tests();
+            printf( "\n" );
+            CU_basic_show_failures( CU_get_failure_list() );
+            printf( "\n\n" );
+            rv = CU_get_number_of_tests_failed();
+        }
+
+        CU_cleanup_registry();
+
+    }
+
+    return rv;
+}
+
