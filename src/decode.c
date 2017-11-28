1c1a0b2f3de319aa66c2c5ba1a96650ca78d65a6
mander207 <Minas_Anderiasian@cable.comcast.com>
2017-11-28 14:52:17 -0800

    change strdup() to strndup().

--- src/decode.c
+++ src/decode.c
@@ -262,14 +262,12 @@ int process_map(msgpack_object_map *map, schedule_event_t **t)
 
 int decode_string_type (msgpack_object *key, msgpack_object *val, schedule_t **t)
 {
-    char temp[128];
     (void ) key;
 
-    memset(temp, 0, 128);
-    strncpy(temp, val->via.str.ptr, val->via.str.size);
-    (*t)->time_zone = strdup(temp);
+    (*t)->time_zone = strndup(val->via.str.ptr, val->via.str.size);
     debug_info("time_zone:%s\n", (*t)->time_zone);
     (void ) set_unix_time_zone((*t)->time_zone);
+
     return 0;
 }
 
